8dd421575c50806fd53e8a12c39d57db685f677b
Ajmal junaid <ajmal6201@gmail.com>
2023-11-02 08:04:35 +0530

    done with different cases

--- App.js
+++ App.js
@@ -5,13 +5,13 @@ export default function App() {
   return (
     // <View style={{backgroundColor:"plum", flex: 1}}></View>
     <View style={styles.container}>
-      {/* <Box style={{backgroundColor: "#8e9b90",flex: 1}}>Box 1</Box>
-      <Box style={{backgroundColor: "#b69b30", flex: 1}}>Box 2</Box>
-      <Box style={{backgroundColor: "#1c9b00", flex: 3}}>Box 3</Box>
-      <Box style={{backgroundColor: "#ab3b00"}}>Box 4</Box> */}
-      <Box style={{backgroundColor: "#b99b00",paddingVertical:100}}>Box 5</Box>
-      <Box style={{backgroundColor: "#8e9b00",paddingEnd: 90}}>Box 6</Box>
-      <Box style={{backgroundColor: "#8e5a40",paddingBottom:60}}>Box 7</Box> 
+      <Box style={{backgroundColor: "#8e9b90", alignSelf:"flex-end"}}>Box 1</Box>
+      <Box style={{backgroundColor: "#b69b30", alignSelf:"flex-start"}}>Box 2</Box>
+      <Box style={{backgroundColor: "#1c9b00", alignSelf:"center"}}>Box 3</Box>
+      <Box style={{backgroundColor: "#ab3b00", alignSelf:"stretch"}}>Box 4</Box>
+      <Box style={{backgroundColor: "#b99b00", alignSelf:"auto"}}>Box 5</Box>
+      <Box style={{backgroundColor: "#8e9b00"}}>Box 6</Box>
+      <Box style={{backgroundColor: "#8e5a40"}}>Box 7</Box> 
     </View>
   );
 }
@@ -21,9 +21,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex:1,
-    flexDirection:"row",
-    //justifyContent: "center",
-    alignItems: "baseline",
+    alignItems:"flex-end",
+    // flexDirection:"row",
+    // //justifyContent: "center",
+    // alignItems: "baseline",
     marginTop:64,
     borderWidth: 6,
     borderColor: "red",
