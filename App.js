0ad7826f888ca8a0290df321cfb0f553a715f29f
Ajmal junaid <ajmal6201@gmail.com>
2023-11-02 07:55:39 +0530

    baseline understand

--- App.js
+++ App.js
@@ -9,9 +9,9 @@ export default function App() {
       <Box style={{backgroundColor: "#b69b30", flex: 1}}>Box 2</Box>
       <Box style={{backgroundColor: "#1c9b00", flex: 3}}>Box 3</Box>
       <Box style={{backgroundColor: "#ab3b00"}}>Box 4</Box> */}
-      <Box style={{backgroundColor: "#b99b00"}}>Box 5</Box>
-      <Box style={{backgroundColor: "#8e9b00"}}>Box 6</Box>
-      <Box style={{backgroundColor: "#8e5a40"}}>Box 7</Box> 
+      <Box style={{backgroundColor: "#b99b00",paddingVertical:100}}>Box 5</Box>
+      <Box style={{backgroundColor: "#8e9b00",paddingEnd: 90}}>Box 6</Box>
+      <Box style={{backgroundColor: "#8e5a40",paddingBottom:60}}>Box 7</Box> 
     </View>
   );
 }
@@ -21,9 +21,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex:1,
-    // flexDirection:"row",
-    justifyContent: "center",
-    alignItems: "center",
+    flexDirection:"row",
+    //justifyContent: "center",
+    alignItems: "baseline",
     marginTop:64,
     borderWidth: 6,
     borderColor: "red",
