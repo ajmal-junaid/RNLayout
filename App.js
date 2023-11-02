f5e156af04eb5f0fa34264a78597b4f7e9d9e2c9
Ajmal junaid <ajmal6201@gmail.com>
2023-11-02 07:34:40 +0530

    done

--- App.js
+++ App.js
@@ -8,10 +8,10 @@ export default function App() {
       <Box style={{backgroundColor: "#8e9b90",flex: 1}}>Box 1</Box>
       <Box style={{backgroundColor: "#b69b30", flex: 1}}>Box 2</Box>
       <Box style={{backgroundColor: "#1c9b00", flex: 3}}>Box 3</Box>
-      <Box style={{backgroundColor: "#ab3b00"}}>Box 4</Box>
+      {/* <Box style={{backgroundColor: "#ab3b00"}}>Box 4</Box>
       <Box style={{backgroundColor: "#b99b00"}}>Box 5</Box>
       <Box style={{backgroundColor: "#8e9b00"}}>Box 6</Box>
-      <Box style={{backgroundColor: "#8e5a40"}}>Box 7</Box>
+      <Box style={{backgroundColor: "#8e5a40"}}>Box 7</Box> */}
     </View>
   );
 }
@@ -21,6 +21,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex:1,
+    flexDirection:"row-reverse",
     marginTop:64,
     borderWidth: 6,
     borderColor: "red",
